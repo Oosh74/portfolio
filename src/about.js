f611f62b49d44347be453ef479dd5bf07f6bcacb
Jared <usherjared@yahoo.com>
2021-01-19 13:18:07 -0500

    added hover effect to images, added tool tips, cleaned up some CSS

--- src/about.js
+++ src/about.js
@@ -12,14 +12,24 @@ function About() {
           <p className="aboutText">
             I am fullstack software engineer from New Jersey {'\n'}specializing
             in React, Node, Express, and PostgreSQL. {'\n'}I have a passion for
-            webdevelopment, learning, and solving{'\n'}unique problems using
+            web development, learning, and solving{'\n'}unique problems using
             modern technologies.{'\n'}I am always striving to improve and create
             high quality projects!
           </p>
-          {/* <div> PLACEHOLDER </div> */}
+          <div className="skillsHeader">
+            <h1> Skills </h1>
+            <img
+              src={
+                'https://icongr.am/octicons/arrow-down.svg?size=100&color=4f6290'
+              }
+              alt="downarrow"
+              className="skillsArrow"
+            />
+          </div>
         </div>
         <div className="imageGrid">
           <div className="imageContainer">
+            <p> JavaScript </p>
             <img
               src={
                 'https://icongr.am/devicon/javascript-original.svg?size=75&color=000000                '
@@ -28,6 +38,7 @@ function About() {
             />
           </div>
           <div className="imageContainer">
+            <p> React </p>
             <img
               src={
                 'https://icongr.am/devicon/react-original.svg?size=75&color=000000'
@@ -36,84 +47,97 @@ function About() {
             />
           </div>
           <div className="imageContainer">
+            <p> Express </p>
+
             <img
               src={
                 'https://icongr.am/devicon/express-original-wordmark.svg?size=75&color=000000'
               }
               alt="express"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Node </p>
             <img
               src={
                 'https://icongr.am/devicon/nodejs-original.svg?size=75&color=000000'
               }
               alt="node"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> PostgreSQL </p>
             <img
               src={
                 'https://icongr.am/devicon/postgresql-original.svg?size=75&color=000000'
               }
               alt="postgresql"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Sequelize </p>
             <img
               src={
                 'https://icongr.am/devicon/sequelize-original.svg?size=75&color=000000'
               }
               alt="sequelize"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Git </p>
             <img
               src={
                 'https://icongr.am/devicon/git-original.svg?size=75&color=000000'
               }
               alt="git"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Heroku </p>
             <img
               src={
                 'https://icongr.am/devicon/heroku-original.svg?size=75&color=000000'
               }
               alt="heroku"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> HTLM5 </p>
             <img
               src={
                 'https://icongr.am/devicon/html5-original.svg?size=75&color=000000'
               }
               alt="html"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> CSS3 </p>
             <img
               src={
                 'https://icongr.am/devicon/css3-original.svg?size=75&color=000000'
               }
               alt="css"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Socket.IO </p>
             <img src={socketLogo} alt="socketio" />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Redux </p>
             <img src={reduxLogo} alt="redux" />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Github </p>
             <img
               src={
                 'https://icongr.am/devicon/github-original.svg?size=75&color=currentColor'
               }
               alt="github"
             />
-          </div>{' '}
+          </div>
           <div className="imageContainer">
+            <p> Bootstrap </p>
             <img
               src={
                 'https://icongr.am/devicon/bootstrap-plain.svg?size=75&color=currentColor'
