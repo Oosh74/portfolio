f42a022be2c6de58be6f3ad6b8d998cf6a81312f
Jared <usherjared@yahoo.com>
2021-09-07 21:48:32 -0400

    Re-did scrolling effects on navbar to be dynamic

--- src/navbar.js
+++ src/navbar.js
@@ -1,100 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import _ from 'underscore';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Navbar = () => {
-  const [buttonScroll, setButtonScroll] = useState(false);
-  const [section, setSection] = useState({
-    home: 'active',
-    about: 'inactive',
-    port: 'inactive',
-    cont: 'inactive',
-  });
+const Navbar = (props, headerProp) => {
+  const headerRef = useRef(null);
+  const [visibleSection, setVisibleSection] = useState();
 
-  const ininitalState = {
-    home: 'inactive',
-    about: 'inactive',
-    port: 'inactive',
-    cont: 'inactive',
+  const getDimensions = (ele) => {
+    const { height } = ele.getBoundingClientRect();
+    const offsetTop = ele.offsetTop;
+    const offsetBottom = offsetTop + height;
+    return {
+      height,
+      offsetTop,
+      offsetBottom,
+    };
   };
 
-  const { home, about, port, cont } = section;
-
-  const handleScroll = () => {
-    if (
-      window.scrollY >= 0 &&
-      window.scrollY <= 639 &&
-      home === 'inactive' &&
-      buttonScroll === false
-    ) {
-      setSection({ ...ininitalState, home: 'active' });
-    } else if (
-      window.scrollY >= 640 &&
-      window.scrollY <= 1309 &&
-      about === 'inactive' &&
-      buttonScroll === false
-    ) {
-      setSection({ ...ininitalState, about: 'active' });
-    } else if (
-      window.scrollY >= 1310 &&
-      window.scrollY < 1969 &&
-      port === 'inactive' &&
-      buttonScroll === false
-    ) {
-      setSection({ ...ininitalState, port: 'active' });
-    } else if (
-      window.scrollY >= 1970 &&
-      cont === 'inactive' &&
-      buttonScroll === false
-    ) {
-      setSection({ ...ininitalState, cont: 'active' });
-    }
+  const scrollTo = (ele) => {
+    ele.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   };
 
   useEffect(() => {
-    const throttledCount = _.throttle(handleScroll, 100);
-    window.addEventListener('scroll', throttledCount, { passive: true });
-    return () =>
-      window.removeEventListener('scroll', throttledCount, { passive: true });
-  });
+    const handleScroll = () => {
+      const { height: headerHeight } = getDimensions(headerRef.current);
+      const scrollPosition = window.scrollY + headerHeight;
 
-  const handleClick = (button, x, y) => {
-    setButtonScroll(true);
+      const selected = props.props.find(({ section, ref }) => {
+        const ele = ref.current;
+        if (ele) {
+          const { offsetBottom, offsetTop } = getDimensions(ele);
+          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
+        }
+      });
 
-    if (button === 1 && home === 'inactive') {
-      setSection({ ...ininitalState, home: 'active' });
-    } else if (button === 2) {
-      setSection({ ...ininitalState, about: 'active' });
-    } else if (button === 3) {
-      setSection({ ...ininitalState, port: 'active' });
-    } else if (button === 4) {
-      setSection({ ...ininitalState, cont: 'active' });
-    }
+      if (selected && selected.section !== visibleSection) {
+        setVisibleSection(selected.section);
+      } else if (!selected && visibleSection) {
+        setVisibleSection(undefined);
+      }
+    };
 
-    window.scrollTo({
-      top: x,
-      left: y,
-      behavior: 'smooth',
-    });
-
-    setTimeout(function () {
-      setButtonScroll(false);
-    }, 750);
-  };
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [visibleSection]);
 
   return (
-    <div className="nav-bar">
+    <div className="nav-bar" ref={headerRef}>
       <nav className="nav-links">
-        <button className={home} onClick={() => handleClick(1, 0, 0)}>
-          Home
+        <button
+          type="button"
+          className={`navLink ${
+            visibleSection === 'Landing' ? 'selected' : ''
+          }`}
+          onClick={() => {
+            scrollTo(props.props[0].ref.current);
+          }}
+        >
+          HOME
         </button>
-        <button className={about} onClick={() => handleClick(2, 660, 0)}>
-          About
-        </button>
-        <button className={port} onClick={() => handleClick(3, 1329, 0)}>
-          Portfolio
-        </button>
-        <button className={cont} onClick={() => handleClick(4, 1998, 0)}>
-          Contact
+        <button
+          type="button"
+          className={`${visibleSection === 'About' ? 'selected' : ''}`}
+          onClick={() => {
+            scrollTo(props.props[1].ref.current);
+          }}
+        >
+          ABOUT
+        </button>{' '}
+        <button
+          type="button"
+          className={`${visibleSection === 'Portfolio' ? 'selected' : ''}`}
+          onClick={() => {
+            scrollTo(props.props[2].ref.current);
+          }}
+        >
+          PORTFOLIO
+        </button>{' '}
+        <button
+          type="button"
+          className={`${visibleSection === 'Contact' ? 'selected' : ''}`}
+          onClick={() => {
+            scrollTo(props.props[3].ref.current);
+          }}
+        >
+          CONTACT
         </button>
       </nav>
     </div>
